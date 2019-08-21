cb7e76587293027989dd0e1b18326c6894e85213
Sorin Chis <kish.sorin@yahoo.com>
2019-08-21 09:55:29 +0300

    added webpack and introduction folder

---
+++ about webpack/introduction.js
@@ -0,0 +1,12 @@
+
+/*
+     What is Webpack
+
+     Webpack takes all of your assets and outputs them to a production-ready bundle
+
+          - Bundles Javascript files into one file 
+          - Process SASS / LESS files into CSS and only use them where needed
+          - Convert JSX or ES2015 into vanilla JS which browsers understand
+
+
+*/
